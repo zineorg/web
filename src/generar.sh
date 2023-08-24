4b9dea532ae88a9e37bc0d6d34a3a98024917a9d
zineorg <pmiramon@protonmail.com>
2023-08-25 00:50:06 +0200

    Nuevos nombres menú, nueva presentación 3

--- src/generar.sh
+++ src/generar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for f in ../es/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c src/style.css -B menu.html -A pie.html -o "../$(basename "${f%.*}").html"; done
+for f in ../raw/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c src/style.css -B menu.html -A pie.html -o "../$(basename "${f%.*}").html"; done
