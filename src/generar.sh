b71e8ccff5e23ac080e23fcaeb513aa5e181a861
zineorg <pmiramon@protonmail.com>
2023-08-31 17:00:16 +0200

    prueba arbol

--- src/generar.sh
+++ src/generar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 for f in ../*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c src/style.css -B menu.html -A pie.html -o "../$(basename "${f%.*}").html"; done
-for f in ../geofono/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c ../src/style.css -B menu.html -A pie.html -o "../geofono/$(basename "${f%.*}").html"; done
+for f in ../geofono/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c src/style.css -B menu.html -A pie.html -o "../geofono/$(basename "${f%.*}").html"; done
+for f in ../microfono1/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c src/style.css -B menu.html -A pie.html -o "../camara1/$(basename "${f%.*}").html"; done
