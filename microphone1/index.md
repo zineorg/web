f7416dcb58469fd349d154e2e4da58fae4d8b51a
zineorg <pmiramon@protonmail.com>
2023-11-02 13:32:56 +0100

    traduccion de la web al ingles, incompleta

---
+++ microphone1/index.md
@@ -0,0 +1,26 @@
+---
+title: Microphone1 ~ Zine
+description: Zine open-source microphones.
+date: 02/11/2023
+lang: en
+author: pmiramon
+keywords: zine, cinema, free, software, hardware, field, recording, sound, design
+header-includes: <style>.intro{display:none;} .idioma{display:none;} .cuerpo{max-width:95%;} a.seleccion.microfono1::before{content:"➞ "; font-weight:bolder;}</style>
+rights: Creative Commons Attribution-ShareAlike 4.0 International License
+---
+
+::: presentacion
+![](/microfono1/img/microfono1.svg){#logo}
+
+These small, high-sensitivity, low-noise stereo microphones are perfect for field recordings. They have an omnidirectional pattern and have a high resistance to interference. They are based on the AOM-5024L electret condenser capsule, plus a 3.5mm Neutrik minijack connector and Mogami W2368 miniature cable with a diameter of only 2mm. The body is printed in recycled PET with carbon fibers and its design always maintains the proper distance between the capsule and the windshield, always keeping an air chamber that significantly reduces the effect of wind on the capsule. Although it is recommended to use them with an audio recorder, they can also be used with any DSLR camera or device that has a 3.5mm audio input with PIP.
+:::
+
+::: botonera
+
+[Instructions](instructions.html){.boton}
+
+[3D Files](/microfono1/3D/microfono.FCStd){.boton}
+
+[Help](help.html){.boton}
+
+:::
--- src/generar.sh
+++ src/generar.sh
@@ -7,6 +7,11 @@ for f in ../geofono/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantill
 for f in ../camara/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c /src/style.css -B menu.html -A pie.html -o "../camara/$(basename "${f%.*}").html"; done
 for f in ../notas/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c /src/style.css -B menu.html -A pie.html -o "../notas/$(basename "${f%.*}").html"; done
 for f in ../en/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c /src/style.css -B menu-en.html -A pie-en.html -o "../en/$(basename "${f%.*}").html"; done
+for f in ../microphone1/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c /src/style.css -B menu-en.html -A pie-en.html -o "../microphone2/$(basename "${f%.*}").html"; done
+for f in ../microphone2/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c /src/style.css -B menu-en.html -A pie-en.html -o "../microphone2/$(basename "${f%.*}").html"; done
+for f in ../geophone/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c /src/style.css -B menu-en.html -A pie-en.html -o "../geophone/$(basename "${f%.*}").html"; done
+for f in ../camera/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c /src/style.css -B menu-en.html -A pie-en.html -o "../camera/$(basename "${f%.*}").html"; done
+for f in ../notes/*.md; do pandoc "$f" --toc --toc-depth=3 --template plantilla.html -c /src/style.css -B menu-en.html -A pie-en.html -o "../notes/$(basename "${f%.*}").html"; done
 rm ../README.html
 git add ../ -A
 echo "Introduce el mensaje para el commit:"
